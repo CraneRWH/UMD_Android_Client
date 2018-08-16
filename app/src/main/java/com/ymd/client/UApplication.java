6fcc7bae72e74985c180fed446ea099e94623b5f
zhanghongliang <zhl188115>
2018-08-16 11:09:08 +0800

    Create project,first commit.

---
+++ app/src/main/java/com/ymd/client/UApplication.java
@@ -0,0 +1,12 @@
+package com.ymd.client;
+
+import android.app.Application;
+
+/**
+ * @author zhl
+ * @class com.ymd.client
+ * @time 2018/8/16 0016 10:53
+ * @description
+ */
+public class UApplication extends Application{
+}
--- app/src/main/java/com/ymd/client/component/activity/MainActivity.java
+++ app/src/main/java/com/ymd/client/component/activity/MainActivity.java
@@ -1,8 +1,13 @@
-package com.ymd.client;
+package com.ymd.client.component.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.ymd.client.R;
+
+/**
+ * 主界面
+ */
 public class MainActivity extends Activity {
 
     @Override
