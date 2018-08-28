5fd8705754229541afe8b8875e083dfb0e58dc0b
rongweihe <rong_whe@massclouds.com>
2018-08-28 09:39:47 +0800

    美事

--- app/src/main/java/com/ymd/client/component/activity/homePage/food/FoodListFragment.java
+++ app/src/main/java/com/ymd/client/component/activity/homePage/food/FoodListFragment.java
@@ -4,12 +4,19 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ymd.client.R;
+import com.ymd.client.component.adapter.food.FoodListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -59,7 +66,71 @@ public class FoodListFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_food_list, container, false);
         unbinder = ButterKnife.bind(this, view);
+        initView();
         return view;
+    }
+
+    private void initView() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        FoodListAdapter adapter = new FoodListAdapter(getData(), getContext());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List getData() {
+        List<Map<String,Object>> list = new ArrayList<>();
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "半天妖烤鱼");
+        map.put("distance","253m");
+        map.put("point",4.3);
+        map.put("work_time", "9:00~21:30");
+        map.put("dis_str","全场");
+        map.put("dis_num", "8.6折");
+        map.put("price", 86);
+        map.put("unit","人");
+        List<String> diss = new ArrayList<>();
+        diss.add("全场8.9折优惠");
+        diss.add("早餐免费领豆浆");
+        diss.add("上午9:00至12:00有7折优惠");
+        map.put("diss", diss);
+
+        list.add(map);
+
+        map = new HashMap<>();
+        map.put("name", "驴肉火烧");
+        map.put("distance","253m");
+        map.put("point",4.3);
+        map.put("work_time", "9:00~21:30");
+        map.put("dis_str","全场");
+        map.put("dis_num", "8.6折");
+        map.put("price", 86);
+        map.put("unit","人");
+        diss = new ArrayList<>();
+        diss.add("全场8.9折优惠");
+        diss.add("早餐免费领豆浆");
+        diss.add("上午9:00至12:00有7折优惠");
+        map.put("diss", diss);
+
+        list.add(map);
+
+        map = new HashMap<>();
+        map.put("name", "沙县小吃");
+        map.put("distance","253m");
+        map.put("point",4.3);
+        map.put("work_time", "9:00~21:30");
+        map.put("dis_str","全场");
+        map.put("dis_num", "8.6折");
+        map.put("price", 86);
+        map.put("unit","人");
+        diss = new ArrayList<>();
+        diss.add("全场8.9折优惠");
+        diss.add("早餐免费领豆浆");
+        diss.add("上午9:00至12:00有7折优惠");
+        map.put("diss", diss);
+
+        list.add(map);
+
+        return list;
     }
 
     @Override
